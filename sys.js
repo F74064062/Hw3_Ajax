87873dadfea9aadd922473fe71cd0761958e4920
Leo Wang <56250432+F74064062@users.noreply.github.com>
2021-03-23 23:27:40 +0800

    get_ID_complete

--- sys.js
+++ sys.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
     $('#listall button').click((event) => {
       event.preventDefault()
-      $.getJSON("./students", function(data) {
+      $.getJSON("./students.json", function(data) {
         $("#list_all").empty();
         $.each(data, function(key, value){
-          $("#list_all").append('${key}: ${value} <br>')
+          $("#list_all").append(`${key}: ${value} <br>`)
         })
       });
-    })  
+    })
+    
+    
+    
   });
