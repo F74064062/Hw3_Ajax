26f4d4ab3cc9de59d6adf998d66a009b360df32f
Leo Wang <56250432+F74064062@users.noreply.github.com>
2021-05-22 14:35:35 +0800

    Add files via upload

--- sys.js
+++ sys.js
@@ -1,3 +1,26 @@
+var list_btn = document.getElementById("list_btn");
+var search_btn = document.getElementById("search_btn");
+var add_btn = document.getElementById("add_btn");
+var delete_btn = document.getElementById("delete_btn");
+var list_all = document.getElementById("list_all");
+var student_search = document.getElementById("student_search");
+
+
+
+listbtn.addEventListener("click", function(){
+  $.getJSON("./students.json", function(data){
+    $.each(data, function(key, value){
+      $("#list_all").append(`${key}: ${value} <br>`)
+    })
+  });
+});
+
+
+
+
+
+/*
+
 $(document).ready(function() {
     
   $('#listall button').click((event) => {
@@ -22,3 +45,5 @@ $(document).ready(function() {
     
     
   });
+
+  */
