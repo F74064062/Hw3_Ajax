b36ed755e5f7b04c78d33bab55df6fce858a69bb
Leo Wang <56250432+F74064062@users.noreply.github.com>
2021-05-22 22:53:48 +0800

    Add files via upload

--- ser.js
+++ ser.js
@@ -1,16 +1,50 @@
 
+// Step 1 and step 2 code goes here
+
 const express = require('express')
-
 const app = express()
+const port = 15849
+const fs = require('fs');
 
-const port = 17586
 
+/*
+app.get('/step1', (req, res) => {
+       res.send('<h1>hello world</h1>')
+})
+*/
 app.listen(port, () => {
   console.log(`listening on port: ${port}`)
 })
 
-app.use(express.static(`${__dirname}`))
-
-app.get('/hello', (req, res) => {
-  res.send(`Hello, ${req.query.student_ID}`)
+// Step 3 code goes here
+app.use(express.static(`${__dirname}/dist`))
+//step 4
+/*
+let nRequests = 0
+app.get('/step4', (req, res) => {
+  res.send(`this is request #${++nRequests}`)
 })
+
+app.get('/step5', (req, res) => {
+  res.send(`Hello, ${req.query.fname} ${req.query.lname}`)
+})
+*/
+app.get('/getlist', (req,res) =>{
+  fs.readFile('./student.json', 'uft-8',(err, jsonString) =>{
+    if(!err){
+      res.send('jsonString')
+    }
+  })
+})
+
+/*
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+app.post('/step7', (req, res) => {
+  res.send(`Hello, ${req.body.fname} ${req.body.lname}`)
+})
+*/
--- sys.js
+++ sys.js
@@ -1,13 +1,51 @@
+$(document).ready(function() {
+  $('#list button[type="submit"]').click((event) => {
+    event.preventDefault()
+    $.get('./getlist', {},(data) => {$('#ajax-output').html(data)})
+  })
+
+/*
+  $('#ajax-form button[type="submit"]').click((event) => {
+    event.preventDefault()
+    // Step 9 and step 10 code goes here
+    $.get('./step5', {
+          fname: $('#ajax-form [name=fname]').val(),
+          lname: $('#ajax-form [name=lname]').val(),
+          }, (data) => {
+          $('#list_all').html(data)
+        })
+  })
+  */
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 var list_btn = document.getElementById("list_btn");
 var search_btn = document.getElementById("search_btn");
 var add_btn = document.getElementById("add_btn");
 var delete_btn = document.getElementById("delete_btn");
 var list_all = document.getElementById("list_all");
 var student_search = document.getElementById("student_search");
+var student_search = document.getElementById("student_search")
 
 
-
-listbtn.addEventListener("click", function(){
+list_btn.addEventListener("click", function(){
+  $("#list_all").empty();
   $.getJSON("./students.json", function(data){
     $.each(data, function(key, value){
       $("#list_all").append(`${key}: ${value} <br>`)
@@ -15,11 +53,35 @@ listbtn.addEventListener("click", function(){
   });
 });
 
+list_btn.addEventListener("click", function(){
+  var student_ID = document.getElementById("student_ID").value;
+  $.getJSON("./students.json", function(data){
+    $.each(data, function(key, value){
+      if(key == student_ID)
+        $("#student_search").append(`Hello, ${value}`)
+    })
+  });
+});
 
+list_btn.addEventListener("click", function(){
+  var student_ID1 = document.getElementById("student_ID1").value;
+  var student_name = document.getElementById("student_name").value;
+  const fs = require('fs');
+  var data = fs.readFileSync('students.json');
+  var myObject= JSON.parse(data);
+  var newData = {
+    student_ID1 : student_name
+  }
+  myObject.push(newData);
+  var newData = JSON.stringify(myObject);
+  fs.writeFile('data.json', newData, err => {
+    // error checking
+    if(err) throw err;
+    
+  console.log("New data added");
+  });  
+});
 
-
-
-/*
 
 $(document).ready(function() {
     
